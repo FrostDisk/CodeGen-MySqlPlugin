fcbfaa0c3b7188dd175624a183c63562e36e9359
NioZero <ghidalgo@frostdisk.com>
2017-08-28 11:00:37 -0300

    Uploading first Version

---
+++ CodeGen-MySql/src/CodeGen.AccessModel.MySql/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodeGen.AccessModel.MySql")]
+[assembly: AssemblyDescription("FrostDisk CodeGen AccessModel Plugin for MySql")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FrostDisk")]
+[assembly: AssemblyProduct("CodeGen.AccessModel.MySql")]
+[assembly: AssemblyCopyright("Copyright FrostDisk© 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2efac270-7409-46dd-b9a1-9e605a71f349")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.0.*")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
